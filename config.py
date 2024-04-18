2050b26c5adf73e24d765e506853b7941463bb84
Sergey Udachin <s.udachin@mail.ru>
2024-04-18 16:46:31 +0300

    feat: add alembic config, add initial migration

---
+++ config.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# extract DB related env variables
+DB_HOSTNAME = os.getenv("DB_HOSTNAME")
+DB_PORT = os.getenv("DB_PORT")
+
+DB_NAME = os.getenv("DB_NAME")
+
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
