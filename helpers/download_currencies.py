732f34b026012c56cd640095d1c422c446de1b02
Sergey Udachin <s.udachin@mail.ru>
2024-05-05 17:41:22 +0300

    chore: cleanup naming in download_currencies helper

--- helpers/download_currencies.py
+++ helpers/download_currencies.py
@@ -11,9 +11,9 @@ def get_html_from_page(url: str) -> str:
 def get_unique_codes_names(html_page: str) -> dict:
     codes_names_dict = dict()
     soup = BeautifulSoup(html_page, "html.parser")
-    rows = soup.find_all("tr")
-    for row in rows:
-        columns = row.find_all("td")
+    table_rows = soup.find_all("tr")
+    for table_row in table_rows:
+        columns = table_row.find_all("td")
         if len(columns) == 4:
             country, name, cur_code = columns[0].text, columns[1].text, columns[2].text
             if len(cur_code) == 3 and codes_names_dict.get(cur_code) is None:
@@ -22,9 +22,9 @@ def get_unique_codes_names(html_page: str) -> dict:
 
 
 def save_to_csv(filename: str, codes_names_dict: dict) -> None:
-    with open(filename, "w", newline="") as csvfile:
+    with open(filename, "w", newline="") as csv_file:
         fieldnames = ["code", "fullname"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
         for code, fullname in codes_names_dict.items():
             writer.writerow({"code": code, "fullname": fullname})
