7c73c4ecc3c8e8b6ecb29976774c38a63a247a20
Sergey Udachin <s.udachin@mail.ru>
2024-05-05 13:30:38 +0300

    feat: add currency codes loader, currency codes CSV resource file

---
+++ helpers/download_currencies.py
@@ -0,0 +1,43 @@
+import csv
+import httpx
+from bs4 import BeautifulSoup
+
+
+def get_html_from_page(url: str) -> str:
+    response = httpx.get(url)
+    return response.text
+
+
+def get_unique_codes_names(html_page: str) -> dict:
+    codes_names_dict = dict()
+    soup = BeautifulSoup(html_page, "html.parser")
+    rows = soup.find_all("tr")
+    for row in rows:
+        columns = row.find_all("td")
+        if len(columns) == 4:
+            country, name, cur_code = columns[0].text, columns[1].text, columns[2].text
+            if len(cur_code) == 3 and codes_names_dict.get(cur_code) is None:
+                codes_names_dict[cur_code] = name
+    return codes_names_dict
+
+
+def save_to_csv(filename: str, codes_names_dict: dict) -> None:
+    with open(filename, "w", newline="") as csvfile:
+        fieldnames = ["code", "fullname"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        for code, fullname in codes_names_dict.items():
+            writer.writerow({"code": code, "fullname": fullname})
+
+
+CURRENCY_CODES_SOURCE_PAGE = "https://www.iban.com/currency-codes"
+
+if __name__ == "__main__":
+    html = get_html_from_page(CURRENCY_CODES_SOURCE_PAGE)
+    unique_codes = get_unique_codes_names(html)
+    save_to_csv("../resources/codes_currencies.csv", unique_codes)
+
+    with open("../resources/codes_currencies.csv", newline="") as csvfile:
+        rows = csv.reader(csvfile, delimiter=",")
+        for row in rows:
+            print(row)
