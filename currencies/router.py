022436b7f851cf268b6f225a3a5571f25c0a1605
Sergey Udachin <s.udachin@mail.ru>
2024-05-13 15:05:13 +0300

    feat: add currency DELETE endpoint. convert currency codes to uppercase

--- currencies/router.py
+++ currencies/router.py
@@ -1,9 +1,11 @@
 import logging
+import typing
 from sqlite3 import IntegrityError
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Path, status, HTTPException
-from sqlalchemy import select, insert
+from fastapi import APIRouter, Depends, status, HTTPException, Path
+from pydantic import Field
+from sqlalchemy import select, insert, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import IntegrityError
 
@@ -29,14 +31,38 @@ async def get_currencies(session: AsyncSession = Depends(get_async_session)):
 async def post_currency(
     curr: Currency, session: AsyncSession = Depends(get_async_session)
 ):
+    curr.code = curr.code.upper()
     stmt = (insert(currency).values(**curr.dict())).returning(currency)
     try:
         async with session:
             response = await session.execute(stmt)
             await session.commit()
     except IntegrityError:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Currency already exists")
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, detail="Currency already exists"
+        )
     return response.mappings().fetchone()
+
+
+@currencies_router.delete("/", status_code=status.HTTP_202_ACCEPTED)
+async def delete_currency(
+    code: typing.Annotated[str, Field(str, min_length=3, max_length=3)],
+    session: AsyncSession = Depends(get_async_session),
+):
+    code = code.upper()
+    stmt = (delete(currency).where(currency.c.code == code)).returning(currency)
+
+    async with session:
+        response = await session.execute(stmt)
+        await session.commit()
+
+    response = response.mappings().fetchone()
+    if not response:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Currency with code {code} not found",
+        )
+    return response
 
 
 @currency_router.get("/{currency_code}")
